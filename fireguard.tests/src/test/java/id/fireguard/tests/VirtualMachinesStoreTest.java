f79e3785d79da578cea8488baabd9903e1e8cb31
lambdaprime <intid@protonmail.com>
2020-03-21 15:37:31 -0700

    Adding tests subproject

---
+++ fireguard.tests/src/test/java/id/fireguard/tests/VirtualMachinesStoreTest.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2020 lambdaprime
+ */
+
+package id.fireguard.tests;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import id.fireguard.vmm.VirtualMachineEntity;
+import id.fireguard.vmm.VirtualMachinesStore;
+
+public class VirtualMachinesStoreTest {
+
+    @Test
+    public void test_happy() {
+        Path store = Paths.get("/tmp/store" + System.currentTimeMillis());
+        System.out.println(store);
+        VirtualMachinesStore pm = VirtualMachinesStore.load(store);
+        VirtualMachineEntity entity1 = new VirtualMachineEntity();
+        entity1.setHomeFolder("/tmp/lol");
+        entity1.setSocket("/tmp/sock");
+        pm.add(entity1);
+
+        VirtualMachineEntity entity2 = new VirtualMachineEntity();
+        entity2.setHomeFolder("/tmp/lol2");
+        entity2.setSocket("/tmp/sock1");
+        pm.add(entity2);
+
+        pm.save();
+
+        pm = VirtualMachinesStore.load(store);
+        var vms = pm.findAll();
+        Assertions.assertEquals(1, vms.size());
+        Assertions.assertEquals("id: null\nhome folder: /tmp/lol\nsocket: /tmp/sock\nstate: null\npid: null\n",
+            vms.get(0).toString());
+    }
+}
--- fireguard/src/main/java/module-info.java
+++ fireguard/src/main/java/module-info.java
@@ -1,4 +1,6 @@
 module id.fireguard {
     requires id.xfunction;
     requires com.google.gson;
+    
+    exports id.fireguard.vmm to id.fireguard.tests;
 }
