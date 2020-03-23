191ffdbc6eea7dc42155223a48548216fb5ab59c
lambdaprime <intid@protonmail.com>
2020-03-23 01:34:07 +0000

    Introducing Dhcpd class to manage interactions with dhcpd

--- fireguard.tests/src/test/java/module-info.java
+++ fireguard.tests/src/test/java/module-info.java
@@ -1,7 +1,9 @@
 module id.fireguard.tests {
     exports id.fireguard.tests;
+    opens id.jnix.tests.net.dhcpd;
     
     requires id.fireguard;
     requires org.junit.jupiter.api;
     requires org.junit.jupiter.params;
+	requires id.xfunction;
 }
---
+++ fireguard/src/main/java/id/jnix/net/dhcpd/Subnet.java
@@ -0,0 +1,27 @@
+package id.jnix.net.dhcpd;
+
+import java.net.InetAddress;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Subnet {
+
+	InetAddress subnet;
+	InetAddress netmask;
+	List<Host> hosts = List.of();
+	
+	public Subnet(InetAddress subnet, InetAddress netmask, Host...hosts) {
+		this.subnet = subnet;
+		this.netmask = netmask;
+		this.hosts = Arrays.asList(hosts);
+	}
+
+	@Override
+	public String toString() {
+		return String.format("subnet %s netmask %s { %s }", subnet.getHostAddress(),
+				netmask.getHostAddress(),
+				hosts.stream().map(Host::toString)
+				.collect(Collectors.joining("\n")));
+	}
+}
