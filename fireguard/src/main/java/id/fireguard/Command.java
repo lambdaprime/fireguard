aa96ba7db55e704cedd70e29d726320b6ee98f0e
lambdaprime <id.blackmesa@gmail.com>
2020-03-25 21:59:26 -0700

    No need to expose LinkedList since removeFirst can be replaced with remove(0)

--- fireguard/src/main/java/id/fireguard/Command.java
+++ fireguard/src/main/java/id/fireguard/Command.java
@@ -1,9 +1,9 @@
 package id.fireguard;
 
-import java.util.LinkedList;
+import java.util.List;
 
 public interface Command {
 
-	void execute(LinkedList<String> positionalArgs) throws CommandIllegalArgumentException;
+	void execute(List<String> positionalArgs) throws CommandIllegalArgumentException;
 
 }
