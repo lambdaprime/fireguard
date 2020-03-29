43f2073cf38f3441df8bf757941f0aee0bfd256f
lambdaprime <intid@protonmail.com>
2020-03-28 18:07:56 -0700

    Adding IpGenerator + tests

---
+++ fireguard.tests/src/test/java/id/fireguard/tests/net/IpGeneratorTests.java
@@ -0,0 +1,85 @@
+package id.fireguard.tests.net;
+
+import static java.net.InetAddress.getByName;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.net.InetAddress;
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import id.fireguard.net.IpGenerator;
+
+public class IpGeneratorTests extends IpGenerator {
+
+    @Test
+    public void test_inc() throws Exception {
+    	String actual = inc(getByName("123.2.3.0")).get().toString();
+		assertEquals("/123.2.3.1", actual);
+		
+		actual = inc(getByName("123.2.3.254")).get().toString();
+		assertEquals("/123.2.4.1", actual);
+
+		actual = inc(getByName("123.2.254.254")).get().toString();
+		assertEquals("/123.3.1.1", actual);
+
+		actual = inc(getByName("123.2.254.253")).get().toString();
+		assertEquals("/123.2.254.254", actual);
+    }
+
+    @Test
+    public void test_cmpIp() throws Exception {
+		assertEquals(1, cmpIp(getByName("123.2.3.254"), getByName("123.2.3.253")));
+		assertEquals(0, cmpIp(getByName("123.2.3.254"), getByName("123.2.3.254")));
+		assertEquals(-256, cmpIp(getByName("123.2.4.3"), getByName("123.2.5.3")));
+    }
+
+    @Test
+    public void test_valid() throws Exception {
+		assertTrue(valid(getByName("123.2.3.0"), getByName("123.2.3.254")));
+		assertTrue(valid(getByName("123.2.0.0"), getByName("123.2.3.254")));
+		assertFalse(valid(getByName("123.2.3.0"), getByName("123.2.5.254")));
+    }
+
+    @Test
+    public void test_nextIp_from_1() throws Exception {
+    	Map<InetAddress, Set<InetAddress>> ipPool = Map.of(
+    			getByName("123.2.3.0"), Set.of(getByName("123.2.3.254")));
+    	var actual = nextIp(ipPool, getByName("123.2.3.0")).get().toString();
+    	assertEquals("/123.2.3.1", actual);
+    }
+
+    @Test
+    public void test_nextIp_wrong_subnet() throws Exception {
+    	Map<InetAddress, Set<InetAddress>> ipPool = Map.of(
+    			getByName("123.2.3.0"), Set.of(getByName("123.2.3.254")));
+    	Assertions.assertThrows(AssertionError.class,
+				() -> nextIp(ipPool, getByName("123.2.3.12")));
+    }
+
+    @Test
+    public void test_nextIp_after_1() throws Exception {
+    	Map<InetAddress, Set<InetAddress>> ipPool = Map.of(
+    			getByName("123.2.3.0"), Set.of(
+    					getByName("123.2.3.1"),
+    					getByName("123.2.3.254")));
+    	var actual = nextIp(ipPool, getByName("123.2.3.0")).get().toString();
+    	assertEquals("/123.2.3.2", actual);
+    }
+
+    @Test
+    public void test_nextIp_consecutive() throws Exception {
+    	Map<InetAddress, Set<InetAddress>> ipPool = Map.of(
+    			getByName("123.2.3.0"), Set.of(
+    					getByName("123.2.3.4"),
+    					getByName("123.2.3.3"),
+    					getByName("123.2.3.2"),
+    					getByName("123.2.3.1")));
+    	var actual = nextIp(ipPool, getByName("123.2.3.0")).get().toString();
+    	assertEquals("/123.2.3.5", actual);
+    }
+}
---
+++ fireguard/src/main/java/id/fireguard/net/IpGenerator.java
@@ -0,0 +1,64 @@
+package id.fireguard.net;
+
+import java.net.InetAddress;
+import java.nio.ByteBuffer;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import id.xfunction.XAsserts;
+import id.xfunction.function.Unchecked;
+
+public class IpGenerator {
+
+	public Optional<InetAddress> nextIp(Map<InetAddress, Set<InetAddress>> ipPool, InetAddress subnet) {
+		XAsserts.assertTrue(subnet.getAddress()[3] == 0, "Wrong subnet format");
+		return nextIp(subnet, ipPool.getOrDefault(subnet, Set.of()));
+	}
+
+	private Optional<InetAddress> nextIp(InetAddress subnet, Set<InetAddress> ips) {
+		var ip = inc(subnet);
+		if (ip.isEmpty()) return Optional.empty();
+		var sorted = ips.stream()
+			.sorted(this::cmpIp)
+			.collect(Collectors.toList());
+		var iter = sorted.iterator();
+		while (iter.hasNext()) {
+			if (!ip.get().equals(iter.next())) return ip;
+			ip = inc(ip.get());
+			if (ip.isEmpty()) return Optional.empty();
+		}
+		if (!valid(subnet, ip.get())) {
+			return Optional.empty();
+		}
+		return ip;
+	}
+	
+	protected boolean valid(InetAddress subnet, InetAddress ip) {
+		var sb = subnet.getAddress();
+		var ipb = ip.getAddress();
+		for (int i = 0; i < 4; i++) {
+			if (sb[i] == 0) continue;
+			if (sb[i] != ipb[i]) return false;
+		}
+		return true;
+	}
+
+	protected Optional<InetAddress> inc(InetAddress ip) {
+		var b = ip.getAddress();
+		for (int i = 3; i >= 0; i--) {
+			if (Byte.compareUnsigned(b[i], (byte)254) >= 0) {
+				b[i] = 1;
+				continue;
+			}
+			b[i]++;
+			return Unchecked.get(() -> Optional.of(InetAddress.getByAddress(b)));
+		}
+		return Optional.empty();
+	}
+
+	protected int cmpIp(InetAddress a, InetAddress b) {
+		return ByteBuffer.wrap(a.getAddress()).getInt() - ByteBuffer.wrap(b.getAddress()).getInt();
+	}
+}
