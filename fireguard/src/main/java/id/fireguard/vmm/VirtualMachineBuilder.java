148d62f17366cb96902606b2a6f8e49e6f6a2ab3
lambdaprime <id.blackmesa@gmail.com>
2020-03-29 21:02:45 -0700

    Removing pid from VirtualMachine ctor

--- fireguard/src/main/java/id/fireguard/vmm/VirtualMachineBuilder.java
+++ fireguard/src/main/java/id/fireguard/vmm/VirtualMachineBuilder.java
@@ -11,22 +11,28 @@ public class VirtualMachineBuilder {
     private VmConfigUtils configUtils = new VmConfigUtils();
 
     public VirtualMachine build(String id, State state, Path home, Path socket, Optional<Long> pid) {
+    	var vm = build(id, state, home, socket);
+    	pid.ifPresent(p -> vm.withPid(p));
+    	return vm;
+    }
+
+    public VirtualMachine build(String id, State state, Path home, Path socket) {
         VmConfig vmConfig = configUtils.load(home);
         return new VirtualMachine(id,
             state,
             home,
             socket,
-            vmConfig,
-            pid);
+            vmConfig);
     }
 
     public VirtualMachine buildWithNewConfig(VirtualMachine vm) {
         VmConfig vmConfig = configUtils.load(vm.getHome());
-        return new VirtualMachine(vm.getId(),
+        var newvm = new VirtualMachine(vm.getId(),
             vm.getState(),
             vm.getHome(),
             vm.getSocket(),
-            vmConfig,
-            vm.getPid());
+            vmConfig);
+        vm.getPid().ifPresent(p -> newvm.withPid(p));
+        return newvm;
     }
 }
