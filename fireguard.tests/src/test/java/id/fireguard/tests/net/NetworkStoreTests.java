61789e585f343e71945044a1ca63270ec19c88c9
lambdaprime <id.blackmesa@gmail.com>
2020-03-29 21:15:42 -0700

    NetworkStore::update does not update the object if it already exist

---
+++ fireguard.tests/src/test/java/id/fireguard/tests/net/NetworkStoreTests.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2020 lambdaprime
+ */
+
+package id.fireguard.tests.net;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import id.fireguard.net.NetworkEntity;
+import id.fireguard.net.NetworkStore;
+import id.fireguard.tests.ObjectStoreMock;
+
+public class NetworkStoreTests {
+
+	private NetworkStore nm;
+
+	@BeforeEach
+	void setup() {
+		nm = new NetworkStore(new ObjectStoreMock<>());
+	}
+	
+    @Test
+    public void test_update() throws UnknownHostException {
+    	String id = "net1";
+		var net1 = new NetworkEntity(id, null, null);
+    	nm.add(net1);
+    	InetAddress subnet = InetAddress.getLocalHost();
+		var net2 = new NetworkEntity(id, subnet, null);
+    	nm.update(net2);
+    	Assertions.assertEquals(subnet, nm.findNet(id).get().getSubnet());
+    }
+
+}
