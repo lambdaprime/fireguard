66c7176e35d9d473ed3aa87ad2bfb81037ae0470
lambdaprime <id.blackmesa@gmail.com>
2020-04-03 17:49:10 -0700

    Fixing config not notifying the listeners on updates

---
+++ fireguard.tests/src/test/java/id/fireguard/tests/net/NetworkManagerConfigTests.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2020 lambdaprime
+ */
+
+package id.fireguard.tests.net;
+
+import java.net.UnknownHostException;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import id.fireguard.net.NetworkManagerConfig;
+
+public class NetworkManagerConfigTests {
+	
+    @Test
+    public void test_setLastId() throws UnknownHostException {
+    	boolean[] isNotified = new boolean[1];
+    	var nmc  = new NetworkManagerConfig();
+    	nmc.addListener(val -> {
+    		isNotified[0] = true;
+    	});
+    	nmc.setLastId(12);
+    	Assertions.assertTrue(isNotified[0]);
+    }
+
+    @Test
+    public void test_setLastUsedMacAddr() throws UnknownHostException {
+    	boolean[] isNotified = new boolean[1];
+    	var nmc  = new NetworkManagerConfig();
+    	nmc.addListener(val -> {
+    		isNotified[0] = true;
+    	});
+    	nmc.setLastUsedMacAddr(null);
+    	Assertions.assertTrue(isNotified[0]);
+    }
+}
