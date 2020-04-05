e16cdf20040348a2dfa1075860ad503b54234e93
lambdaprime <id.blackmesa@gmail.com>
2020-04-04 19:38:15 -0700

    Moving from GSON to Jq

--- fireguard/src/main/java/id/fireguard/vmm/vmconfig/VmConfigJsonUtils.java
+++ fireguard/src/main/java/id/fireguard/vmm/vmconfig/VmConfigJsonUtils.java
@@ -1,26 +1,51 @@
 package id.fireguard.vmm.vmconfig;
 
-import java.io.IOException;
-import java.nio.file.Files;
+import static java.util.stream.Collectors.toList;
+
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Optional;
 
-import com.google.gson.Gson;
+import id.jnix.Jq;
+import id.xfunction.XAsserts;
+import id.xfunction.XProcess;
 
 public class VmConfigJsonUtils {
 
     public VmConfigJson load(Path location) {
         try {
-        	Gson gson = new Gson();
-        	var vmConfig = gson.fromJson(Files.newBufferedReader(location), VmConfigJson.class);
-        	//System.out.println(gson.toJson(vmConfig));
-            return vmConfig;
-        } catch (IOException e) {
+        	var machineConfig = machineConfig(location);
+        	var networkInterfaces = new ArrayList<NetworkIface>();
+        	networkIface(location).ifPresent(networkInterfaces::add);
+            return new VmConfigJson(machineConfig, networkInterfaces);
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
     
-    public static void main(String[] args) {
-		new VmConfigJsonUtils().load(Paths.get("/home/ubuntu/vms/orc/stage/vm-2/vm_config.json"));
+    private Optional<NetworkIface> networkIface(Path location) throws Exception {
+    	var proc = new Jq()
+	    		.withFile(location)
+	    		.withFilter(".\"network-interfaces\"[0]| .\"guest_mac\",.\"host_dev_name\",.\"iface_id\"| select (.!=null)")
+	    		.run();
+    	var out = new XProcess(proc).stdout().collect(toList());
+    	if (out.isEmpty()) return Optional.empty();
+    	XAsserts.assertTrue(out.size() == 3, "Unknown network-interfaces");
+    	return Optional.of(new NetworkIface(out.get(2), out.get(0), out.get(1)));
+	}
+
+	private MachineConfig machineConfig(Path location) throws Exception {
+    	var proc = new Jq()
+	    		.withFile(location)
+	    		.withFilter(".\"machine-config\"| .\"vcpu_count\",.\"mem_size_mib\"| select (.!=null)")
+	    		.run();
+    	var out = new XProcess(proc).stdout().collect(toList());
+    	XAsserts.assertTrue(out.size() == 2, "Unknown machine-config");
+    	return new MachineConfig(Integer.parseInt(out.get(0)), Integer.parseInt(out.get(1)));
+	}
+
+	public static void main(String[] args) {
+		new VmConfigJsonUtils().load(Paths.get("/home/lynx/vms/origs/ubuntu/vm_config.json"));
 	}
 }
