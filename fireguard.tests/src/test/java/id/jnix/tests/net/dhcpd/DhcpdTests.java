34c08bf27888627331b403cc1e7b3249c1cb2ef9
lambdaprime <intid@protonmail.com>
2020-04-23 22:39:38 -0700

    Store config file location inside of DhcpdConfig  object

--- fireguard.tests/src/test/java/id/jnix/tests/net/dhcpd/DhcpdTests.java
+++ fireguard.tests/src/test/java/id/jnix/tests/net/dhcpd/DhcpdTests.java
@@ -23,16 +23,18 @@ public class DhcpdTests {
 
     @Test
     public void test_config() throws Exception {
-        DhcpdConfig conf = new DhcpdConfig(new SubnetSection(getByName("172.16.0.0"),
-                getByName("255.255.0.0"),
-                new Host("vm1", "AA:FC:00:00:00:01", getByName("172.16.1.10"))));
+        DhcpdConfig conf = new DhcpdConfig("/tmp/dhcpd.conf",
+                    new SubnetSection(getByName("172.16.0.0"),
+                            getByName("255.255.0.0"),
+                            new Host("vm1", "AA:FC:00:00:00:01", getByName("172.16.1.10"))));
         assertEquals(XUtils.readResource(getClass(), "dhcpd.config"), conf.toString());
     }
 
     public static void main(String[] args) throws Exception {
-        DhcpdConfig conf = new DhcpdConfig(new SubnetSection(getByName("172.16.0.0"),
-                getByName("255.255.0.0"),
-                new Host("vm1", "AA:FC:00:00:00:01", getByName("172.16.1.10"))));
+        DhcpdConfig conf = new DhcpdConfig("/tmp/dhcpd.conf",
+                new SubnetSection(getByName("172.16.0.0"),
+                        getByName("255.255.0.0"),
+                        new Host("vm1", "AA:FC:00:00:00:01", getByName("172.16.1.10"))));
         XProcess proc = new XProcess(new Dhcpd().start(conf));
         System.out.println(proc.stderrAsString());
     }
